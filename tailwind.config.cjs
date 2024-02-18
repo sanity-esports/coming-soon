441732a3ec15169a9c798b1b9ca8eaab3830e9e6
dinesh <talwadkerdinesh@gmail.com>
2024-02-18 13:58:11 +0530

    taos

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	safelist: [
+		'!duration-[0ms]',
+		'!delay-[0ms]',
+		'html.js :where([class*="taos:"]:not(.taos-init))'
+	  ],
+	  content: {
+		relative: true,
+		transform: (content) => content.replace(/taos:/g, ''),
+		files: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	  },
 	darkMode: "class",
 	theme: {
 		extend: {
@@ -19,5 +28,7 @@ module.exports = {
 			}
 		},
 	},
-	plugins: [],
-}
+	plugins: [
+		require('taos/plugin')
+	  ],
+	}
